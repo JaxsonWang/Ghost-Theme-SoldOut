0463352311a8ae8e595c1296c03eebcf357caa86
Jaxson Wang <i@iiong.com>
2020-12-24 15:55:34 +0800

    ⛏ [2020.12.24] 完善功能

--- src/js/bootstrap/waline.js
+++ src/js/bootstrap/waline.js
@@ -1,28 +1,25 @@
 import { loadScripts } from '../utils'
 
 export default () => {
-  if (document.getElementById('vcomments') !== null) {
-    loadScripts([
-      {
-        name: 'waline-js',
-        path: 'https://cdn.jsdelivr.net/npm/@waline/client@0.9.0/dist/Waline.min.js'
-      }
-    ]).then(() => {
-      // eslint-disable-next-line no-undef
-      new Waline({
-        el: '#vcomments',
-        serverURL: 'https://iiong-comments.vercel.app',
-        avatar: 'mm',
-        visitor: true,
-        highlight: true,
-        recordIP: true,
-        placeholder: '请您理智发言，共建美好社会！',
-        path: window.location.pathname,
-        meta: ['nick', 'mail', 'link'],
-        pageSize: 10,
-        lang: 'zh-CN',
-        avatarForce: false
-      })
+  loadScripts([
+    {
+      name: 'waline-js',
+      path: 'https://cdn.jsdelivr.net/npm/@waline/client@0.9.0/dist/Waline.min.js'
+    }
+  ]).then(() => {
+    new Waline({
+      el: '#vcomments',
+      serverURL: 'https://comments.iiong.com',
+      avatar: 'mm',
+      visitor: true,
+      highlight: true,
+      recordIP: true,
+      placeholder: '请您理智发言，共建美好社会！',
+      path: window.location.pathname,
+      meta: ['nick', 'mail', 'link'],
+      pageSize: 10,
+      lang: 'zh-CN',
+      avatarForce: false
     })
-  }
+  })
 }
